082aab1d927d28195e832d2d8631cd4f004fc8a7
i74l0 <i7alog4m3r1@gmail.com>
2025-04-25 08:31:35 -0300

    Adicionei o Thymeleaf

--- contratos/src/main/java/com/getinfo/contratos/controller/MainController.java
+++ contratos/src/main/java/com/getinfo/contratos/controller/MainController.java
@@ -1,18 +1,15 @@
 package com.getinfo.contratos.controller;
 
+import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/")
+@Controller
 public class MainController {
     @GetMapping
-    public String index() {
-        return "<html>"+
-        "<ul>"+
-        "<li><a href='http://localhost:8080/empresas'>Empresas</a></li>"+
-        "<li><a href='http://localhost:8080/contratos'>Contratos</a></li>"+
-        "<li><a href='http://localhost:8080/funcionarios'>Funcionarios</a></li>"+
-        "</ul>"+
-        "</html>";
+    public String home(Model model) {
+        model.addAttribute("message", "Bem-vindo ao Thymeleaf!");
+        return "home";
     }
 }
